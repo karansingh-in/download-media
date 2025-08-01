2f754285f0415149a8eb6fe3aaf5898f2bc19cd4
Karan Singh <singh.karangkp@gmail.com>
2025-08-01 16:52:51 +0530

    added audio download button

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import yt_dlp
 
-# VIDEO DOWNLOADER FROM YOUTUBE
+# VIDEO DOWNLOADER 
 def yt_in_best(url, quality = "1080"):
     opts = {
         'format': 'best',
@@ -10,34 +10,7 @@ def yt_in_best(url, quality = "1080"):
     with yt_dlp.YoutubeDL(opts) as me:
         me.download([url])
 
-
-    
-# def yt_in_720(url, quality = "720"):
-#     opts = {
-#         'format': f'best[height<={quality}]/best',
-#     }
- 
-#     with yt_dlp.YoutubeDL(opts) as me:
-#         me.download([url])
-    
-# def yt_in_480(url, quality = "480"):
-#     opts = {
-#         'format': f'best[height<={quality}]/best',
-#     }
- 
-#     with yt_dlp.YoutubeDL(opts) as me:
-#         me.download([url])
-
-# def yt_in_360(url, quality = "360"):
-#     opts = {
-#         'format': f'best[height<={quality}]/best',
-#     }
- 
-#     with yt_dlp.YoutubeDL(opts) as me:
-#         me.download([url])
-    
-        
-# AUDIO DOWNLOADER FROM YOUTUBE
+# AUDIO DOWNLOADER 
 def audio_from_yt(url):
     ffmpeg_path = os.path.join(os.path.dirname(__file__), 'ffmpeg_file', 'bin')
     opts = {
@@ -52,12 +25,6 @@ def audio_from_yt(url):
     
     with yt_dlp.YoutubeDL(opts) as me:
         me.download([url])
-
-# mus = audio_from_yt('https://youtu.be/8BhYqOC79M4?si=XmuPuDNJkifoYUd5')
-
-# vid_best = yt_in_best('https://youtu.be/8BhYqOC79M4?si=XmuPuDNJkifoYUd5')
-
-# vid360 = yt_in_360('https://youtu.be/8BhYqOC79M4?si=XmuPuDNJkifoYUd5')
 
 
 mus = audio_from_yt('https://pin.it/5iiPy8Bmf')
